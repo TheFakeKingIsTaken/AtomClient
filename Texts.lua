46498b9b7a6417743e1651d8ac92c82ff918790d
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2023-02-21 13:05:23 +0100

    Create Texts.lua

---
+++ Texts.lua
@@ -0,0 +1,8 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Warning";
+    Text = "Atom Client Temporarily recording, for now you can enjoy the SmokeX."; 
+    Duration = 20;
+})
+
+game.StarterGui:SetCore("ChatMakeSystemMessage",  { Text = "SmokeX Discord", Color = Color3.fromRGB(255, 255, 255), Font = Enum.Font.Merriweather, FontSize = Enum.FontSize.Size24 } )
+game.StarterGui:SetCore("ChatMakeSystemMessage",  { Text = "dsc.gg/smokex", Color = Color3.fromRGB(255, 255, 255), Font = Enum.Font.Merriweather, FontSize = Enum.FontSize.Size24 } )
