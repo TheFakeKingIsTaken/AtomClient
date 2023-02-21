845b70cd8422553f69ca0bc394ef7343f8bdacd5
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2023-02-21 13:05:56 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/AtomClient/main/AtomClientDownload.lua", true))()
+
+if game.PlaceId == 6872265039 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loader.lua", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/AtomClient/main/Texts.lua", true))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loader.lua", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/AtomClient/main/Texts.lua", true))()
+end
